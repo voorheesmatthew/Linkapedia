90160bc3f3a232a1a4b9b9d68b5cfcb0f26a5676
matthew voorhees <voorhees.matthew@gmail.com>
2018-11-01 11:45:38 -0700

    Add Color to balls

--- app/index.js
+++ app/index.js
@@ -7,11 +7,22 @@ import 'styles/index.scss';
 import startNode from './start_node';
 
 document.addEventListener("DOMContentLoaded", () => {
+  // let width = 700;
+  // let height = 700;
+
+  // let svg = d3
+  //     .select("#wikiverse")
+  //     .append("svg").attr("id", "svg")
+  //     .attr("height", height)
+  //     .attr("width", width)
+  //     .append("g")
+  //     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
   // Starts the game
   const startButton = document.querySelector("#start");
   startButton.addEventListener("click", () => {
     const startInput = document.querySelector("#si").value;
     startNode(startInput);
+
   });
-  
+
 });
--- app/start_node.js
+++ app/start_node.js
@@ -1,43 +1,51 @@
 // import wikiLinks from './wiki_links';
 
-async function startNode(startName) {
-
+async function startNode(startName, prevNodes = []) {
+  
   let doc = await wtf.fetch(startName);
-  let startNodeLinks = doc.links()
+  let assignedColor = '#' + (Math.random() * 0xFFFFFF << 0).toString(16)
+  let startNodeLinks = doc.links().map(l => ({ page: l.page, origin: startName, color: assignedColor }))
   console.log(startNodeLinks)
-  // debugger
-
-  let width = 500;
-  let height = 500;
-
-  let svg = d3.select("#wikiverse")
-    .append("svg")
-    .attr("height", height)
-    .attr("width", width)
-    .append("g")
-    .attr("transform", "translate(0,0)");
-
+  allNodes = startNodeLinks.concat(prevNodes)
+  
+  let svg = d3
+      .select("#wikiverse")
+      .append("svg")
+      .attr("id", "svg")
+      .attr("preserveAspectRatio", "xMinYMin meet")
+      .attr("viewBox", "0 0 300 300")
+      .classed("svg-content", true)
+      .append("g")
+      .attr("transform", "translate(0,0)");
+  
   let simulation = d3.forceSimulation()
-    .force("x", d3.forceX(width / 2).strength(0.05))
-    .force("y", d3.forceY(height / 2).strength(0.05))
-    .force("collide", d3.forceCollide(35))
+  .force("x", d3.forceX(150).strength(.03))
+  .force("y", d3.forceY(150).strength(.03))
+  .force("collide", d3.forceCollide(4))
+  
+  let circles = svg.selectAll()
+  .data(allNodes)
+  .enter().append("circle")
+  .attr("class", "nodes")
+  .attr("r", 3)
+  .attr("fill", function(d){
+    return d.color;
+  })
+  .attr("cx", function(d) {
+    return d.x;
+  })
+  .attr("cy", function(d) {
+    return d.y;
+  })
+  .on('mouseover', function(d) {
+    console.log(d)
+  })
+  .on('click', function(d) {
+    document.getElementById("svg").remove()
+    startNode(d.page, allNodes)
+  })
 
-  // d3.queue()
-    // .defer(d3.json, doc.links())
-    // .await(function(error, datapoints) {
-      // debugger
-      let circles = svg.selectAll()
-        .data(startNodeLinks)
-        .enter().append("circle")
-        .attr("class", "start-node")
-        .attr("r", 35)
-        .attr("fill", "lightblue")
-        .attr("cx", 100).attr("cy", 300)
-    // })
-
-
-
-  simulation.nodes(startNodeLinks)
+  simulation.nodes(allNodes)
     .on('tick', ticked);
 
   function ticked() {
