211967beb8299e7ba612532f41d2ee2a1caebdc9
matthew voorhees <voorhees.matthew@gmail.com>
2018-11-02 11:37:04 -0700

    Implement reset function

--- app/index.js
+++ app/index.js
@@ -7,22 +7,15 @@ import 'styles/index.scss';
 import startNode from './start_node';
 
 document.addEventListener("DOMContentLoaded", () => {
-  // let width = 700;
-  // let height = 700;
-
-  // let svg = d3
-  //     .select("#wikiverse")
-  //     .append("svg").attr("id", "svg")
-  //     .attr("height", height)
-  //     .attr("width", width)
-  //     .append("g")
-  //     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-  // Starts the game
+  
   const startButton = document.querySelector("#start");
   startButton.addEventListener("click", () => {
+    if (document.getElementById("svg")) {
+        document.getElementById("svg").remove();
+    }
     const startInput = document.querySelector("#si").value;
-    startNode(startInput);
-
+    // debugger
+    startNode(startInput, [], 150, 150);
   });
 
 });
--- app/start_node.js
+++ app/start_node.js
@@ -1,10 +1,30 @@
 // import wikiLinks from './wiki_links';
 
-async function startNode(startName, prevNodes = []) {
+async function startNode(startName, prevNodes = [], xStart, yStart) {
   
-  let doc = await wtf.fetch(startName);
+  let doc = await wtf.fetch(startName, "en");
   let assignedColor = '#' + (Math.random() * 0xFFFFFF << 0).toString(16)
-  let startNodeLinks = doc.links().map(l => ({ page: l.page, origin: startName, color: assignedColor }))
+  let startNodeLinks;
+  let filter = document.querySelector("#filter").checked;
+  if (filter) {
+    startNodeLinks = doc
+        .sections(1)
+        .links()
+        .map(l => ({
+            page: l.page,
+            origin: startName,
+            color: assignedColor
+        }))
+        .concat(doc
+                .sections(0)
+                .links()
+          .map(l => ({ page: l.page, origin: startName, color: assignedColor })));
+  } else {
+    startNodeLinks = doc.links().map(l => ({ page: l.page, origin: startName, color: assignedColor }))
+  }
+  let prevPages = prevNodes.map(el => el.page);
+  // debugger
+  startNodeLinks = startNodeLinks.filter(el => el.page && !prevPages.includes(el.page))//.slice(0,21)
   console.log(startNodeLinks)
   allNodes = startNodeLinks.concat(prevNodes)
   
@@ -17,11 +37,11 @@ async function startNode(startName, prevNodes = []) {
       .classed("svg-content", true)
       .append("g")
       .attr("transform", "translate(0,0)");
-  
+  // " + xStart + ", " + yStart + "
   let simulation = d3.forceSimulation()
-  .force("x", d3.forceX(150).strength(.03))
-  .force("y", d3.forceY(150).strength(.03))
-  .force("collide", d3.forceCollide(4))
+  .force("x", d3.forceX(xStart).strength(.02))
+  .force("y", d3.forceY(yStart).strength(.02))
+  .force("collide", d3.forceCollide(3.5))
   
   let circles = svg.selectAll()
   .data(allNodes)
@@ -36,15 +56,39 @@ async function startNode(startName, prevNodes = []) {
   })
   .attr("cy", function(d) {
     return d.y;
+    })
+  .text(function (d) { return d.page; })
+  .on('mouseover', mouseover)
+  .on('mousemove', function(d) {
+    return mousemove(d)
   })
-  .on('mouseover', function(d) {
-    console.log(d)
-  })
+  .on('mouseout', mouseout)
   .on('click', function(d) {
     document.getElementById("svg").remove()
-    startNode(d.page, allNodes)
+    document.getElementById("textbox").remove()
+    // debugger
+    startNode(d.page, allNodes, 150, 150)// , d.x, d.y
   })
 
+  let div = d3.select("body").append("div")
+  .attr("class", "textbox")
+  .attr("id", "textbox")
+  .style("display", "none")
+
+  function mouseover() {
+    div.style("display", "inline")
+  }
+
+  function mousemove(d) {
+    div.text(d.page)
+      .style("left", (d3.event.pageX-100) + "px")
+      .style("top", (d3.event.pageY-50) + "px");
+  }
+
+  function mouseout() {
+    div.style("display", "none");
+  }
+  
   simulation.nodes(allNodes)
     .on('tick', ticked);
 
