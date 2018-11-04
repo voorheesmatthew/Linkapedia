6b009e34db4b4236897ed0c9888847357ef51997
matthew voorhees <voorhees.matthew@gmail.com>
2018-11-03 22:49:57 -0700

    Complete style before game mode

--- app/index.js
+++ app/index.js
@@ -9,13 +9,35 @@ import startNode from './start_node';
 document.addEventListener("DOMContentLoaded", () => {
   
   const startButton = document.querySelector("#start");
+  const resetButton = document.querySelector("#reset")
+  
+  
   startButton.addEventListener("click", () => {
-    if (document.getElementById("svg")) {
-        document.getElementById("svg").remove();
+    let startInput = document.querySelector("#si").value.toLowerCase();
+    startInput = startInput.replace(/\b\w/g, l => l.toUpperCase());
+    if (startInput.length) {
+      startButton.setAttribute("class", "hide-btn")
+      resetButton.removeAttribute("class")
+      // credit to https://stackoverflow.com/questions/2332811/capitalize-words-in-string
+      startNode(startInput);
     }
-    const startInput = document.querySelector("#si").value;
-    // debugger
-    startNode(startInput, [], 150, 150);
+  });
+
+  resetButton.addEventListener("click", () => {
+    
+    document.getElementById("svg").remove();
+    let linkBtns = document.getElementsByClassName("link-btn");
+    while (linkBtns[0]) {
+      linkBtns[0].parentNode.removeChild(linkBtns[0]);
+    }
+    let startInput = document.querySelector("#si").value.toLowerCase();
+    startInput = startInput.replace(/\b\w/g, l => l.toUpperCase());
+    if (startInput.length) {
+      startNode(startInput);
+    } else {
+      resetButton.setAttribute("class", "hide-btn");
+      startButton.removeAttribute("class");
+    }
   });
 
 });
--- app/start_node.js
+++ app/start_node.js
@@ -1,88 +1,167 @@
 // import wikiLinks from './wiki_links';
+const colors = ["#fd1790","#8df84d","#c33dd6","#cbe211","#f37ef6","#28de66","#fc3b39","#45facb","#c85286","#1b8f43","#7f8c10","#4eb5f8","#5c82a9","#fa9942","#93e3f4","#d25244","#aff1a3","#f7c6eb","#ac6e52","#f8d9a1","#845c00"];
 
-async function startNode(startName, prevNodes = [], xStart, yStart) {
+async function startNode(startName, prevNodes = [], xStart = 100, yStart = 100) {
   
   let doc = await wtf.fetch(startName, "en");
-  let assignedColor = '#' + (Math.random() * 0xFFFFFF << 0).toString(16)
-  let startNodeLinks;
-  let filter = document.querySelector("#filter").checked;
-  if (filter) {
-    startNodeLinks = doc
-        .sections(1)
-        .links()
-        .map(l => ({
-            page: l.page,
-            origin: startName,
-            color: assignedColor
-        }))
-        .concat(doc
-                .sections(0)
-                .links()
-          .map(l => ({ page: l.page, origin: startName, color: assignedColor })));
+  if (!doc) {
+    let noPageError = document.getElementById("middle")
+    let errorText = document.createTextNode(`Sorry, ${startName} not found. Check your spelling!`);
+    noPageError.appendChild(errorText);
+    setTimeout(() => {
+      noPageError.removeChild(errorText);
+    }, 5000);
+  }
+  let assignedColor;
+  if (colors.length) {
+    assignedColor = colors.shift();
+    colors.push(assignedColor)
   } else {
-    startNodeLinks = doc.links().map(l => ({ page: l.page, origin: startName, color: assignedColor }))
+    assignedColor = '#' + (Math.random() * 0xFFFFFF << 0).toString(16)
   }
   let prevPages = prevNodes.map(el => el.page);
-  // debugger
-  startNodeLinks = startNodeLinks.filter(el => el.page && !prevPages.includes(el.page))//.slice(0,21)
-  console.log(startNodeLinks)
-  allNodes = startNodeLinks.concat(prevNodes)
+
   
+
+
+  if (document.getElementById("textbox")) {
+    document.getElementById("textbox").remove();
+  }
+  if (document.getElementById("svg")) {
+    document.getElementById("svg").remove();
+  }
+
+  
+  
+  let startNodeLinks = [];
+  if (doc) {
+    if (!document.getElementById(startName)) {
+      let pageBtn = document.createElement("button")
+      pageBtn.setAttribute("class", "link-btn");
+      pageBtn.setAttribute("id", startName)
+      pageBtn.setAttribute("onclick", `openNewTab('https://en.wikipedia.org/?curid=${doc.json().pageID}');`);
+      // debugger
+      pageBtn.style.backgroundColor = assignedColor;
+      let t = document.createTextNode(startName);
+      pageBtn.appendChild(t)
+      document.getElementById("key-box").append(pageBtn);
+    }
+    
+ 
+    let filter = document.querySelector("#filter").checked;
+    if (filter) {
+
+      let sectionOne = doc
+        .sections(0)
+        .links()
+        .map(l => ({
+          page: l.page,
+          origin: startName,
+          color: assignedColor,
+          x: xStart,
+          y: yStart,
+          clicked: false,
+          radius: 4
+        }));
+
+      if (doc.sections(1)) {
+        startNodeLinks = sectionOne.concat(doc
+          .sections(1)
+          .links()
+          .map(l => ({
+            page: l.page,
+            origin: startName,
+            color: assignedColor,
+            x: xStart,
+            y: yStart,
+            clicked: false,
+            radius: 4
+          })))
+      } else {
+        startNodeLinks = sectionOne;
+      }
+
+    } else {
+      startNodeLinks = doc.links().map(l => ({ 
+        page: l.page, 
+        origin: startName, 
+        color: assignedColor,
+        x: xStart,
+        y: yStart,
+        clicked: false,
+        radius: 4
+      }))
+    }
+  }
+    
+
+    startNodeLinks = startNodeLinks.filter(el => el.page && !prevPages.includes(el.page))
+    console.log(startNodeLinks)
+    
+
+    allNodes = startNodeLinks.concat(prevNodes)
+
   let svg = d3
-      .select("#wikiverse")
-      .append("svg")
-      .attr("id", "svg")
-      .attr("preserveAspectRatio", "xMinYMin meet")
-      .attr("viewBox", "0 0 300 300")
-      .classed("svg-content", true)
-      .append("g")
-      .attr("transform", "translate(0,0)");
-  // " + xStart + ", " + yStart + "
-  let simulation = d3.forceSimulation()
-  .force("x", d3.forceX(xStart).strength(.02))
-  .force("y", d3.forceY(yStart).strength(.02))
-  .force("collide", d3.forceCollide(3.5))
+    .select("#wikiverse")
+    .append("svg")
+    .attr("id", "svg")
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    .attr("viewBox", "0 0 300 300")
+    .classed("svg-content", true)
+    .append("g")
+    .attr("transform", "translate(0,0)");
+
+  let simulation = d3
+      .forceSimulation()
+      .force("x", d3.forceX(xStart).strength(0.00005))
+      .force("y", d3.forceY(yStart).strength(0.00005))
+      .force("collide", d3.forceCollide(function(d) {
+              return d.radius + 0.5;
+          }))
+      .alpha(100);
   
   let circles = svg.selectAll()
-  .data(allNodes)
-  .enter().append("circle")
-  .attr("class", "nodes")
-  .attr("r", 3)
-  .attr("fill", function(d){
-    return d.color;
-  })
-  .attr("cx", function(d) {
-    return d.x;
-  })
-  .attr("cy", function(d) {
-    return d.y;
+    .data(allNodes)
+    .enter().append("circle")
+    .attr("class", "nodes")
+    .attr("r", function (d) {
+        return d.radius;
     })
-  .text(function (d) { return d.page; })
-  .on('mouseover', mouseover)
-  .on('mousemove', function(d) {
-    return mousemove(d)
-  })
-  .on('mouseout', mouseout)
-  .on('click', function(d) {
-    document.getElementById("svg").remove()
-    document.getElementById("textbox").remove()
-    // debugger
-    startNode(d.page, allNodes, 150, 150)// , d.x, d.y
-  })
+    .attr("fill", function(d){
+      return d.color;
+    })
+    .attr("cx", function(d) {
+      // debugger
+      return d.x;
+    })
+    .attr("cy", function(d) {
+      return d.y;
+      })
+    .text(function (d) { return d.page; })
+    .on('mouseover', mouseover)
+    .on('mousemove', function(d) {
+      return mousemove(d)
+    })
+    .on('mouseout', mouseout)
+    .on('click', function(d) {
+      d.clicked = true;
+      d.radius = d.radius/2;
+      startNode(d.page, allNodes, d.x, d.y)
+    })
 
   let div = d3.select("body").append("div")
-  .attr("class", "textbox")
-  .attr("id", "textbox")
-  .style("display", "none")
+    .attr("class", "textbox")
+    .attr("id", "textbox")
+    .style("display", "none")
 
   function mouseover() {
     div.style("display", "inline")
   }
 
   function mousemove(d) {
-    div.text(d.page)
-      .style("left", (d3.event.pageX-100) + "px")
-      .style("top", (d3.event.pageY-50) + "px");
+    div.text(d.page) //"Page: " + d.origin + " || Link: " +
+        .style("left", d3.event.pageX - 100 + "px")
+        .style("top", d3.event.pageY - 50 + "px");
   }
 
   function mouseout() {
@@ -95,10 +174,13 @@ async function startNode(startName, prevNodes = [], xStart, yStart) {
   function ticked() {
     circles
       .attr("cx", function(d) {
-        return d.x
+        // debugger
+        // simulation
+        return (d.x = Math.max(d.radius, Math.min(300 - d.radius, d.x))); //Width
       })
       .attr("cy", function(d) {
-        return d.y
+        // simulation.alpha(.01)
+        return (d.y = Math.max(d.radius, Math.min(300 - d.radius, d.y)));
       })
   }
 }
